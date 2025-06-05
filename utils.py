e21e8bdc089862bd6616146e04364f45871e8561
quynhbui <93629164+quynhbuiofficial@users.noreply.github.com>
2025-06-05 22:11:11 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,15 @@
 import time
 from pprint import pprint
 from elasticsearch import Elasticsearch
+import os
 
 def get_es_client(max_retries = 1, sleep_time = 1):
     i = 0
     while i < max_retries:
         try:
             # es = Elasticsearch("http://localhost:9200", basic_auth=("elastic", "R5nRHqCLO7HQ9-8O5P*n"))
-            es = Elasticsearch("http://${{ env.ELASTICSEARCH_IP }}:9200")
+            es_ip = os.getenv("ELASTICSEARCH_IP")
+            es = Elasticsearch(f"http://{es_ip}:9200")
             client_info = es.info()
             print("Connected to Elasticsearch! \n")
             pprint(client_info)
